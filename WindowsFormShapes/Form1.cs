f01cbb765b29c22f8c9e86e4fe93a1113cf33c91
Денислав Сунгов <ds70063937@edu.mon.bg>
2023-01-13 19:28:08 +0200

    dobaven triugulnik

--- WindowsFormShapes/Form1.cs
+++ WindowsFormShapes/Form1.cs
@@ -12,12 +12,16 @@ namespace WindowsFormShapes
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            this.CreateGraphics().DrawRectangle(new Pen(Brushes.Green, 5), new Rectangle(randomGenerator.Next(0, this.Width), randomGenerator.Next(0, this.Height), 60, 30));
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            this.CreateGraphics().DrawRectangle(new Pen(Brushes.Green, 5), new Rectangle(randomGenerator.Next(0, this.Width), randomGenerator.Next(0, this.Height), 60, 30));
+            this.CreateGraphics().DrawPolygon(new Pen(Brushes.Red, 5), new Point[] { 
+                new Point(randomGenerator.Next(0, this.Width), randomGenerator.Next(0, this.Height)),
+                new Point(randomGenerator.Next(0, this.Width), randomGenerator.Next(0, this.Height)),
+                new Point(randomGenerator.Next(0, this.Width), randomGenerator.Next(0, this.Height)) 
+            });
         }
 
         private void button3_Click(object sender, EventArgs e)
